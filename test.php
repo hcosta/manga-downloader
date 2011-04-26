8177fe32b7eeca60ba2c40da217994ff9ef68117
Hector Costa <hcostaguzman@gmail.com>
2011-04-26 03:13:01 +0200

    muchos cambios

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 include 'mangaDownloader.php';
 
-/* ESTE EJEMPLO DESCARGA 2 CAPITULOS ESPECIFICOS DE BLEACH VIA ANIMEXTREMIST */
+/* ESTE EJEMPLO DESCARGA 1 CAPITULOS ESPECIFICO VIA ANIMEXTREMIST */
 
 $obj = new mangaDownloader_ax();
 $obj->download('http://www.animextremist.com/mangas-online/mahou-sensei-negima/capitulo-1/mahousenseinegima.html');
@@ -11,18 +11,16 @@ $obj->download('http://www.animextremist.com/mangas-online/mahou-sensei-negima/c
 $obj = new mangaDownloader_sm();
 $obj->download('http://submanga.com/Naruto/533');
 
-/* ESTE EJEMPLO ENVIA UN EMAIL CON EL CAPI ADJUNTO Y EN ZIP A LOS MIEMBROS DE LA LISTA.
-HACE 30 INTENTOS CADA MEDIA HORA HASTA QUE SE PARA Y ENVIA UN EMAIL INFORMANDO QUE NO SE
-HA PUBLICADO. SOLO SE PUEDE USAR EN SERVIDORES CON BASH Y GZIP HABILITADO. LO IDEAL ES
-LLAMAR A ESTE SCRIPT CON EL DEMONIO CRON :-) */
+/* ESTE EJEMPLO DESCARGARA1 CAPITULOS ESPECIFICO DE NARUTO VIA ANIMEREADER */
+
+$obj = new mangaDownloader_mr();
+$obj->download('http://www.mangareader.net/naruto/1'); 
+
+/* ESTE EJEMPLO ENVIA UN EMAIL CON EL CAPI ADJUNTO Y EN ZIP A LOS MIEMBROS DE LA LISTA. **PARA MAS INFO MIRAR EL README */
 
 $users = 'example@example.com, example2@example.com';
 $obj = new mangaDownloader_sm();
 $obj->send_email('http://submanga.com/One_Piece', $users); 
 
-/* ESTE EJEMPLO DESCARGARA1 CAPITULOS ESPECIFICO DE NARUTO VIA ANIMEREADER */
-
-$obj = new mangaDownloader_mr();
-$obj->download('http://www.mangareader.net/naruto/1'); 
 
 ?>
